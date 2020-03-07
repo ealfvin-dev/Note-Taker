3c20e7884152b634b92022667a6ad8313c9c9ec6
Erik Alfvin <erikalfvin@gmail.com>
2020-03-07 10:09:35 -0600

    Got styling fixed. Working on post route

--- server.js
+++ server.js
@@ -7,15 +7,44 @@ var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+notes = [];
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+app.get("/api/notes", function(req, res) {
+    fs.readFile("./db/db.json", function(err, data) {
+        if(err) throw err;
+
+        return data;
+    });
+});
+
+app.post("/api/notes", function(req, res) {
+    console.log("In the server");
+    const newNote = req.body;
+
+    notes.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), function(err) {
+        if(err) throw err;
+    });
+
+    res.json(newNote);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    const id = req.params.id;
+    
+    //Delete note
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
